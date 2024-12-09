862fe47dfd0dd0da152ffc48c6856b6e459c99c0
VladyTol1995 <tolstosheevvladimir4@gmail.com>
2024-12-09 12:03:39 +0300

    Add files via upload

---
+++ modul_1_4_corrected.py
@@ -0,0 +1,5 @@
+number_of_completed_homeworks_assignments = 12
+number_of_hours_spent = 1.5
+name_course = "Python"
+the_time_spent_on_completing_a_single_task = number_of_hours_spent / number_of_completed_homeworks_assignments
+print("Курс:" + name_course + ",","всего задач " + (str(number_of_completed_homeworks_assignments)) + ",","затрачено часов " + (str(number_of_hours_spent)) + "," , "среднее выполнение " + (str(the_time_spent_on_completing_a_single_task) + "."))
